d6bab40e4e44a4edd42ae171791dbcbef645b573
JarJarBass <mason0cullen@gmail.com>
2022-02-14 20:30:02 -0600

    Program runs in full; userSort() has errors.

--- src/HW1_CS2413.cpp
+++ src/HW1_CS2413.cpp
@@ -8,6 +8,11 @@ class CRM {
         int vCounter;
 		int newRowCount; // allows us to track where the new rows begin; as addRow() gets repeatedly called and cleared from the stack, we need to keep track of which value marks a new row.
         static const int EMPTY_ROW = -1; // default value for an empty row.
+
+		int influenceScore(int user);
+		int activityScore(int user);
+        int* userSort(int* scores);
+		
     protected:
         int n; // number of rows in CRM
         int m; // number of columns in CRM
@@ -58,6 +63,10 @@ CRM::CRM(int rows, int cols, int numNonZeros){
 	newRowCount = 0;
 }
 
+int CRM::getNumRows(){
+    return n;
+}
+
 void CRM::addValue(int val){
     values[vCounter] = val;
     vCounter ++;
@@ -76,9 +85,7 @@ if(row > rCounter){ // we've skipped a row and need to fill an EMPTY ROW.
 
 if(row != rCounter - 1){ // checks to see if this is the same as previous row
     for(int i = 0; i < nonZeros; i++){
-        if(values[i] == values[newRowCount]){
-            rowPos[rCounter] = i;
-        }
+        if(values[i] == values[newRowCount]) {rowPos[rCounter] = i;}
      }
     rCounter++;
 }   
@@ -116,11 +123,158 @@ void CRM::display(){
 }
 
 int CRM::mostInfluentialUser(){
-    return 0;
+	// most influential: the user who has been retweeted the most.
+	int currentUser = 0;
+	for(int i = 1; i < n; i ++){
+		if(influenceScore(i) > influenceScore(currentUser))
+			currentUser = i;
+	}
+
+	return currentUser;
+}
+
+int CRM::influenceScore(int user){
+	int userScore = 0;
+	for(int i = 0; i < nonZeros; i++){
+		if(colPos[i] == user){
+			userScore = userScore + values[i];
+		}
+	}
+
+	return userScore;
+}
+
+int CRM::mostActiveUser(){
+	int currentUser = 0;
+
+	for(int i = 0; i < n; i++){
+
+		if(activityScore(i) > activityScore(currentUser))
+			currentUser = i;
+	}
+
+    return currentUser;
+}
+
+int CRM::activityScore(int user){
+
+	int activityScore = 0;
+	int firstIndex = rowPos[user];
+	int lastIndex = firstIndex;
+
+    // If -1, calculating score is simple.
+	if(rowPos[user] == EMPTY_ROW){return activityScore;}
+
+    // executes if "user" is the last entry: simpler case.
+    if(user == n-1){
+        for(int i = rowPos[n-1]; i < nonZeros; i ++)
+            activityScore = activityScore + values[i];
+        
+        return activityScore;
+    }
+
+	// executes for all user IDs other than the last entry: in these cases, a different loop is needed to anticipate potential -1 entries we must pass over.
+	for(int i = 1; i < n; i ++){
+		if(rowPos[user+i] != EMPTY_ROW){
+			lastIndex = rowPos[user+i] - 1;
+			break;
+		}
+	}
+
+    // calculation of actual score.
+	for(int i = firstIndex; i <= lastIndex; i ++) {
+		activityScore = activityScore + values[i];
+	}
+
+	return activityScore;
 }
 
 int* CRM::influentialUsers(){
-    return 0;
+    int* scores = new int[n];
+    int userScore;
+
+    for(int i = 0; i < n; i ++){
+        scores[i] = influenceScore(i);
+    }
+
+    return userSort(scores);
+}
+
+int* CRM::activeUsers(){
+    int* scores = new int[n];
+    int userScore;
+
+    for(int i = 0; i < n; i ++){
+        scores[i] = activityScore(i);
+    }
+
+    return userSort(scores);
+}
+
+// takes an array of scores and returns a list of it's indicies in order based on the score.
+int* CRM::userSort(int* scores){
+    int* userList = new int[n];
+
+    int user;
+    int nextHighest;
+    int score;
+    bool isDuplicate;
+
+    int largestScore = 0;
+
+    // we will aquire the highest score so that we can reference n-1 to easily access the "next highest."
+    for(int i = 0; i < n; i ++){
+            if(scores[i] > largestScore){
+                largestScore = scores[i];
+                userList[0] = i;
+        }
+    }
+
+    for(int i = 1; i < n; i ++){
+        
+        nextHighest = scores[userList[i-1]];
+
+        /*
+        // mark as duplicate value if duplicated AND not already added.
+        for(int k = 0; k < n; k ++){
+            if(scores[i] == scores[k] && i != k){
+                isDuplicate = true;
+            }
+            else{isDuplicate = false;}
+    
+        }
+        */
+       
+        largestScore = 0;
+        for(int k = 0; k < n; k ++){
+
+            if(scores[i] == scores[k] && i != k){
+                isDuplicate = true;
+
+                for(int p = 0; p <= i; p++){
+                    if(k == p) {isDuplicate = false;}
+                }
+            }
+            else{
+                isDuplicate = false;
+            }
+
+            if((scores[k] > largestScore && scores[k] < nextHighest) || isDuplicate){
+                largestScore = scores[k];
+                user = k;
+            }
+        }
+
+        userList[i] = user;
+    }
+
+    /*
+    for(int i = 0; i < n; i ++){
+        cout << "user i: " << i << " | " << "score: " << scores[i] << endl;
+    }
+    */
+
+    return userList;
 }
 
 CRM::~CRM(){
@@ -164,15 +318,34 @@ int main(){
     (*A).display();
 
     // Find most influential user
+	int mostInf = (*A).mostInfluentialUser ();
+	cout << "Most influential user: " << mostInf << endl;
+	cout << endl;
 
     // Find most active user
+	int mostAct = (*A).mostActiveUser();
+	cout << "Most active user: " << mostAct << endl;
+	cout << endl;
 
     // Rank users by their influence
+    int* influentialityVector = (*A).influentialUsers ();
+	cout << "Users ranked by Influentiality: " << endl;
+    for (int i=0; i < (*A).getNumRows ( ); i++) 
+	    cout << influentialityVector [i] << " ";
+    cout << endl << endl;
 
     // Rank users by how active they are
+    cout << "Users ranked by Activity: " << endl;
+    int* activityVector = (*A).activeUsers();
+    for (int i=0; i < (*A).getNumRows ( ); i++) 
+		cout << activityVector[i] << " ";
+    cout << endl;
 
     // CALL DESTRUCTORS
     delete A;
+    delete [] influentialityVector;
+    delete [] activityVector;
+
 
     return 1;
 }
